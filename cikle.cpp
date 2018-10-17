6439c49279308c865009a356a9ba4a50655758cb
DrBorisgab <43994457+DrBorisgab@users.noreply.github.com>
2018-10-17 11:56:54 +0300

    Update cikle.cpp

--- cikle.cpp
+++ cikle.cpp
@@ -4,7 +4,7 @@
 #include <string>
 int main(int argc,char *argv[]) {
     
-    if (argv[1][4] == "1" ) {
+    if (argv[1][0] == '1' ) {
         
         std::string str("<html>\n<svg wight=\"300\" height=\"300\">\n<polygon points = \"100,200 200,140 300,200 200,260\"\n fill=\"none\"\n stroke=\"black\"/>\n</svg>\n</html>");
                 
@@ -16,7 +16,7 @@ int main(int argc,char *argv[]) {
     }
         
                             
-    if (argv[1][4] == "2" ) {
+    if (argv[1][0] == '2' ) {
         
         std::string str("<html>\n<svg widht=\"500\" height=\"500\">\n<polygon points=\"100,100 300,100 300,200 100,200\"\n fill=\"none\"\nstroke=\"black\"/>\n<svg/>\n<html/>");
                                                 
@@ -27,7 +27,7 @@ int main(int argc,char *argv[]) {
                               system("firefox rectangle.html");
     }
                             
-    if (argv[1][4] == "3" ) {
+    if (argv[1][0] == '3' ) {
         std::string str("<html>\n<svg widht=\"500\" height=\"500\">\n<polygon points=\"160,100 300,100 240,180 100,180\"\n fill=\"none\"\n stroke=\"black\"/>\n<svg/>\n<html/>");
                                                   
                                 std::fstream fs;
@@ -39,7 +39,7 @@ int main(int argc,char *argv[]) {
  
                                                            
     
-        if (argv[1][4] == "4" ) {
+        if (argv[1][0] == '4' ) {
             std::string str("<html>\n<svg widht=\"500\" height=\"500\">\n<polygon points=\"100,180 100,140 140,100 260,100 300,140 300,180\"\n fill=\"none\"\n stroke=\"black\"/>\n<polygon points=\"100,280 100,240 300,240 300,280 260,320 140,320\"\n fill=\"none\"\n stroke=\"black\"/>\n<svg/>\n<html/>");
                                 std::fstream fs;
                                 fs.open("polygon.html",std::fstream::out);
